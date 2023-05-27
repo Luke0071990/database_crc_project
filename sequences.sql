5e8260261f89205eb211b3087a907509eef70d2b
Luke0071990 <126398523+Luke0071990@users.noreply.github.com>
2023-05-27 20:50:21 +0200

    Add files via upload

---
+++ sequences.sql
@@ -0,0 +1,49 @@
+-- Tworzenie sekwencji dla tabeli authors
+CREATE SEQUENCE author_id_seq START WITH 1 INCREMENT BY 1;
+
+-- Tworzenie sekwencji dla tabeli suppliers
+CREATE SEQUENCE supplier_id_seq START WITH 1 INCREMENT BY 1;
+
+-- Tworzenie sekwencji dla tabeli customers
+CREATE SEQUENCE customer_id_seq START WITH 1 INCREMENT BY 1;
+
+-- Tworzenie sekwencji dla tabeli employees
+CREATE SEQUENCE employee_id_seq START WITH 1 INCREMENT BY 1;
+
+-- Tworzenie sekwencji dla tabeli products
+CREATE SEQUENCE product_id_seq START WITH 1 INCREMENT BY 1;
+
+-- Tworzenie sekwencji dla tabeli departments
+CREATE SEQUENCE department_id_seq START WITH 1 INCREMENT BY 1;
+
+-- Tworzenie sekwencji dla tabeli publishings
+CREATE SEQUENCE publishing_id_seq START WITH 1 INCREMENT BY 1;
+
+-- Tworzenie sekwencji dla tabeli orders
+CREATE SEQUENCE order_id_seq START WITH 1 INCREMENT BY 1;
+
+-- Tworzenie sekwencji dla tabeli bonuses
+CREATE SEQUENCE bonus_id_seq START WITH 1 INCREMENT BY 1;
+
+-- Tworzenie sekwencji dla tabeli order_payment
+CREATE SEQUENCE payments_seq START WITH 1 INCREMENT BY 1;
+
+-- Tworzenie sekwencji dla tabeli invoices
+CREATE SEQUENCE invoice_id_seq START WITH 1 INCREMENT BY 1;
+
+
+
+/*
+DROP SEQUENCE author_id_seq;
+DROP SEQUENCE supplier_id_seq;
+DROP SEQUENCE customer_id_seq;
+DROP SEQUENCE employee_id_seq;
+DROP SEQUENCE product_id_seq;
+DROP SEQUENCE department_id_seq;
+DROP SEQUENCE publishing_id_seq;
+DROP SEQUENCE order_id_seq;
+DROP SEQUENCE bonus_id_seq;
+DROP SEQUENCE payments_seq;
+DROP SEQUENCE invoice_id_seq;
+DROP SEQUENCE book_cover_id_seq;
+*/
